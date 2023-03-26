a43b0d052134320d944c4cb06c1ce3be22c2cf0b
Nathaniel Simone <122467308+nsimone21@users.noreply.github.com>
2023-03-26 14:37:43 -0400

    Add files via upload

---
+++ MyGenerator.py
@@ -0,0 +1,34 @@
+import os
+import pandas as pd
+import torch
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+Image.MAX_IMAGE_PIXELS = 9999999999
+
+class ImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0]) + '.tif'
+        print(img_path)
+        
+        #Use PIL to open the image. Might not work since it is not a pytorch function.
+        image = Image.open(img_path)
+        
+        #If we want to use read_image from pytorch, image must be png or jpeg.
+        #image = read_image(img_path)
+        
+        label = self.img_labels.iloc[idx, 4]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
